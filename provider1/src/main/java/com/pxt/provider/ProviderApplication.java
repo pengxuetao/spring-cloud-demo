70b6fda278168f749d8883c05726877fb54b9563
pengxuetao <197106340@qq.com>
2020-08-09 17:07:11 +0800

    优化工程pom文件

--- provider1/src/main/java/com/pxt/provider/ProviderApplication.java
+++ provider1/src/main/java/com/pxt/provider/ProviderApplication.java
@@ -2,13 +2,12 @@ package com.pxt.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudProvider1Application.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
 }
--- provider2/src/main/java/com/pxt/provider/ProviderApplication.java
+++ provider2/src/main/java/com/pxt/provider/ProviderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudProvider1Application.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
 }
