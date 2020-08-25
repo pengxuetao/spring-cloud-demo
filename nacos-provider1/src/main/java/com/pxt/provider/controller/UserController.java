4c2f623043590c5c64c5412c4b7b17fd5e583750
Peng xue-tao <197106340@qq.com>
2020-08-25 21:37:09 +0800

    增加3个注册到Nacos的提供者服务

---
+++ nacos-provider1/src/main/java/com/pxt/provider/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.pxt.provider.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.pxt.provider.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Peng xue-tao
+ * @since 2020/8/10
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @HystrixCommand(fallbackMethod = "queryUserException")
+    @GetMapping("/user/{id}")
+    public User queryUser(@PathVariable("id") String id) throws Exception {
+        User user = new User();
+        user.setAddress("provider1");
+        if ("1".equals(id)) {
+            user.setId("1");
+            user.setUserName("一一一");
+        } else {
+            user.setId("999");
+            user.setUserName("九九九");
+        }
+        if (user == null) {
+            throw new Exception("exception");
+        }
+        return user;
+    }
+
+    public User queryUserException(String id) {
+        User user = new User();
+        user.setAddress("provider1");
+        user.setId("error");
+        user.setUserName("exception");
+        return user;
+    }
+}
---
+++ nacos-provider3/src/main/java/com/pxt/provider/NacosProviderApplication.java
@@ -0,0 +1,17 @@
+package com.pxt.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+}
