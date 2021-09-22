9bd5fe2f9d7823a767bee9977c6eaa9bbd4e5c1a
pxt7 <pxt7@qq.com>
2021-09-22 23:34:14 +0800

    添加gateway工程

---
+++ gateway/src/test/java/com/pxt/gateway/GatewayApplicationTest.java
@@ -0,0 +1,12 @@
+package com.pxt.gateway;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GatewayApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
